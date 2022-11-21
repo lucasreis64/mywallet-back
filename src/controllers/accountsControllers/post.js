92c66b150f043cd3fa230eab80103736cd682af3
Lucas <lucasreis64@gmail.com>
2022-11-21 12:49:28 -0300

    feat: date and description post account

--- src/controllers/accountsControllers/post.js
+++ src/controllers/accountsControllers/post.js
@@ -1,21 +1,34 @@
 import { statementsSchema } from "../../models/statementsSchema.js";
 import { validateBySchema } from "../../middlewares/validateBySchema.js";
 import { updateStatement } from "../../models/updateStatement.js";
+import dayjs from "dayjs";
 
 export async function postAccounts(req, res) {
     const { account } = res.locals;
-    const { operation, value } = req.body;
+    const { operation, value, description } = req.body;
     const { statement } = account;
     const id = account._id;
-
+    let now = dayjs();
     if (!validateBySchema(req.body, res, statementsSchema)) return;
 
     statement.forEach((s, idx) => (s.id = idx));
 
     if (operation === "debits")
-        statement.push({ type: "debits", value: value, id: statement.length });
+        statement.push({
+            type: "debits",
+            value: value,
+            description: description,
+            date: now.format("DD/MM"),
+            id: statement.length,
+        });
     else
-        statement.push({ type: "credits", value: value, id: statement.length });
+        statement.push({
+            type: "credits",
+            value: value,
+            description: description,
+            date: now.format("DD/MM"),
+            id: statement.length,
+        });
 
     updateStatement(statement, id, res);
 }
--- src/models/statementsSchema.js
+++ src/models/statementsSchema.js
@@ -2,5 +2,6 @@ import Joi from "joi";
 
 export const statementsSchema = Joi.object({
     operation: Joi.string().valid("debits", "credits").required(),
+    description: Joi.string().max(30),
     value: Joi.number().min(0).required(),
 });
